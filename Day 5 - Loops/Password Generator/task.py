7d9efb40ed6873c096903cb1b2746a87738fc292
WBI\nxg08329 <vikram.avudaiappan@nxp.com>
2024-08-22 16:23:11 +0530

    Day 5 - Loops

---
+++ Day 5 - Loops/Password Generator/task.py
@@ -0,0 +1,53 @@
+"""Password Generator"""
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+print("My Solution")
+print(''.join(random.choices(population=letters, k=nr_letters))
+      + ''.join(random.choices(population=symbols, k=nr_symbols))
+      + ''.join(random.choices(population=numbers, k=nr_numbers)))
+
+print("Solution from course")
+password = ""
+for char in range(0, nr_letters):
+    password += random.choice(letters)
+
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+for char in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+print("Hard Level - Solution from course")
+
+password_list = []
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(password)
