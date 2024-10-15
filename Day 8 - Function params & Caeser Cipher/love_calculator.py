d8e95d3789fe2cc9c9c04537335f8e0b448a2e84
vigkre <vikramvikey30196@gmail.com>
2024-10-15 11:58:18 +0530

    Love calculator solution from course

--- Day 8 - Function params & Caeser Cipher/love_calculator.py
+++ Day 8 - Function params & Caeser Cipher/love_calculator.py
@@ -12,3 +12,26 @@ def calculate_love_score(name1, name2):
     print(f"{true_count}{love_count}")
 
 calculate_love_score("Virat", "Anushka")
+
+# Solution from course
+def calculate_love_score(name1: str, name2:str):
+    combined_names = name1 + name2
+    lower_names = combined_names.lower()
+    
+    t = lower_names.count("t")
+    r = lower_names.count("r")
+    u = lower_names.count("u")
+    e = lower_names.count("e")
+    first_digit = t + r + u + e
+    
+    l = lower_names.count("l")
+    o = lower_names.count("o")
+    v = lower_names.count("v")
+    e = lower_names.count("e")
+    second_digit = l + o + v + e
+    
+    
+    score = int(str(first_digit) + str(second_digit))
+    print(score)
+    
+calculate_love_score("Kanye West", "Kim Kardashian")
