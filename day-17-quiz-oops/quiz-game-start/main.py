df22f6707c03e47d0bd6eb3f5c1c4f9f191affe5
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2025-01-28 23:18:41 +0530

    Day 17 - Quiz program using classes and objects

---
+++ day-17-quiz-oops/quiz-game-start/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank = []
+for question in question_data:
+    new_question = Question(question["text"], question["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+    print("\n\n")
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
