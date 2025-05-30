d6a7af227f5a39fd0b42efcf10f923536db51827
vigkre <vikramvikey30196@gmail.com>
2025-05-30 10:10:09 +0530

    turtle crossing game using OOPS

---
+++ day-23-turtle-crossing-game/player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        """Create a turtle."""
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def is_on_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
+
+    def goto_start(self):
+        self.goto(STARTING_POSITION)
